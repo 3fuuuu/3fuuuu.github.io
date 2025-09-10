6adf783b122dff7d2d55137e27f0e3faa516d509
3fuuuu <fuuuu1009@icloud.com>
2025-09-10 12:25:03 +0900

    button kari

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Provider } from "./components/ui/provider";
-
+import { Button } from "@chakra-ui/react";
 function App({ Component, pageProps }: { Component: any; pageProps: any }) {
   return (
     <Provider>
       <Component {...pageProps} />
+      <Button colorScheme="grean" size="md">
+        Click!
+      </Button>
     </Provider>
   );
 }
