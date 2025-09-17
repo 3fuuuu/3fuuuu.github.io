a38ce934b0dfb6fcb01c65b878d047d2a89fcfa1
3fuuuu <fuuuu1009@icloud.com>
2025-09-17 15:37:24 +0900

    update: linkbutton

--- src/components/ui/LinkButton/LinkButton.tsx
+++ src/components/ui/LinkButton/LinkButton.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export const LinkButton = ({ children, url, onClick }: Props) => {
   return (
-    <Box width="ch" height="" p={2}>
+    <Box width="ch" height="" p={2} maxW="100ch">
       <Link href={url}>
         <Button onClick={onClick} bg="#1f202d" p="10">
           <VStack>{children}</VStack>
