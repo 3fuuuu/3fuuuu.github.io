9664253cc330796e1c3031df88e1631091306b21
3fuuuu <fuuuu1009@icloud.com>
2025-09-24 18:01:09 +0900

    update

---
+++ src/features/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import { Title } from "../../components/ui/Title";
+export const Contact = () => {
+  return (
+    <Box>
+      <Title>
+        <p>Contact</p>
+      </Title>
+    </Box>
+  );
+};
+export default Contact;
