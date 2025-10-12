c2ca35746f199329bcd8371993ead97c7e6d2216
3fuuuu <fuuuu1009@icloud.com>
2025-10-12 20:19:24 +0900

    update

--- src/features/AboutMe/AboutMe.tsx
+++ src/features/AboutMe/AboutMe.tsx
@@ -10,13 +10,14 @@ export const AboutMe = () => {
         <Text color="#9fe1ad" p={3} textStyle="lg">
           愛媛県出身、新居浜工業高等専門学校(24s)所属です。
           <br />
-          本格的なプログラミングは高専に入ってから始めました。高専ではロボット研究部でロボコンに関する制御をしています。また、web(主にフロント)に興味があります。
+          本格的なプログラミングは高専に入ってから始めました。高専ではロボット研究部でロボコンに関する制御をしています。また、webに興味があります。
           <br />
           <Link href="https://yoinokoe.jp/" color="#9fe1ad">
             ヨイノコエクリエイターズグループ
           </Link>
           に所属しています。
         </Text>
+        <Text>More about→</Text>
       </Container>
     </div>
   );
